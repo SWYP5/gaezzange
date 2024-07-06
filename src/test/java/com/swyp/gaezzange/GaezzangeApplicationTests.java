8ba4f55051fe3353f581115d0406db11b4e99d19
batboy118 <118jjh@naver.com>
2024-07-06 21:01:55 +0900

    apply cicd workflow

--- src/test/java/com/swyp/gaezzange/GaezzangeApplicationTests.java
+++ src/test/java/com/swyp/gaezzange/GaezzangeApplicationTests.java
@@ -3,7 +3,7 @@ package com.swyp.gaezzange;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GaezzangeApplicationTests.class)
 class GaezzangeApplicationTests {
 
 	@Test
