3275c0d9cd9ead78102b695bf0499bcb7480a692
batboy118 <118jjh@naver.com>
2024-07-08 20:27:11 +0900

    set cors config

--- src/main/java/com/swyp/gaezzange/configuration/GaezzangeSecurityConfig.java
+++ src/main/java/com/swyp/gaezzange/configuration/GaezzangeSecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +18,7 @@ public class GaezzangeSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(registry ->
                         registry.requestMatchers("/hello").permitAll()
                                 .requestMatchers("/swagger-ui/**").permitAll()
@@ -20,5 +26,16 @@ public class GaezzangeSecurityConfig {
                 );
 
         return http.build();
+    }
+
+    CorsConfigurationSource corsConfigurationSource() {
+        return request -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.setAllowedHeaders(Collections.singletonList("*"));
+            config.setAllowedMethods(Collections.singletonList("*"));
+            config.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://gaejjange.swygbro.com"));
+            config.setAllowCredentials(true);
+            return config;
+        };
     }
 }
