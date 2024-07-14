e9f01801d1298e97ec4a7896882e98983df87054
batboy118 <118jjh@naver.com>
2024-07-15 00:02:43 +0900

    get redirect_url from params for auth

--- src/main/java/com/swyp/gaezzange/authentication/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/swyp/gaezzange/authentication/CustomAuthenticationSuccessHandler.java
@@ -5,9 +5,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -34,14 +36,24 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        String redirectUrl = request.getParameter("redirect_url");
+
+        if (StringUtils.isBlank(redirectUrl)) {
+            throw new ServletException("redirect_url is empty");
+        }
+
         CustomOAuth2User customUserDetails = (CustomOAuth2User) authentication.getPrincipal();
         String accessToken = createJwtToken("access" ,authentication, customUserDetails);
         String refreshToken = createJwtToken("refresh" ,authentication, customUserDetails);
 
         response.addCookie(createCookie("refreshToken", refreshToken, (int) (refreshTokenExpirationTime / 1000)));
         response.setHeader(AUTHORIZATION_HEADER, BEARER_PREFIX + accessToken);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("accessToken", accessToken);
+
         response.setStatus(HttpStatus.OK.value());
-        response.sendRedirect("http://localhost:3000");//테스트후 삭제..?
+        response.sendRedirect(redirectUrl);
     }
 
     private Cookie createCookie(String name, String value, int maxAge) {
