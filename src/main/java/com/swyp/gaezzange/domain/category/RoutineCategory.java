17b5267e7897983acf4ee2370642fb1cc0fae750
batboy118 <118jjh@naver.com>
2024-08-06 11:44:05 +0900

    add routine category

--- src/main/java/com/swyp/gaezzange/domain/category/RoutineCategory.java
+++ src/main/java/com/swyp/gaezzange/domain/category/RoutineCategory.java
@@ -1,10 +1,28 @@
 package com.swyp.gaezzange.domain.category;
 
+import com.swyp.gaezzange.domain.tendency.Tendency;
+import lombok.Getter;
+
 public enum RoutineCategory {
-    WORK_AND_STUDY,
-    HEALTH,
-    SEL_DEVELOPMENT,
-    HOBBY,
-    MEETING,
-    REST
+  WORK_AND_STUDY(Tendency.GAEMI),
+  HEALTH(Tendency.GAEMI),
+  SELF_DEVELOPMENT(Tendency.GAEMI),
+  NETWORKING(Tendency.GAEMI),
+  CERTIFICATION(Tendency.GAEMI),
+  GAEMI_ETC(Tendency.GAEMI),
+
+  REST(Tendency.BAEZZANGE),
+  HOBBY(Tendency.BAEZZANGE),
+  GAME(Tendency.BAEZZANGE),
+  FOOD(Tendency.BAEZZANGE),
+  MUSIC_AND_SHOW(Tendency.BAEZZANGE),
+  MEETING(Tendency.BAEZZANGE),
+  BAEZZANGE_ETC(Tendency.BAEZZANGE);
+
+  @Getter
+  private final Tendency tendency;
+
+  RoutineCategory(Tendency tendency) {
+    this.tendency = tendency
+  }
 }
